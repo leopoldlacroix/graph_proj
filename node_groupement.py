cfbf3c6b3d65206bc7bc0ad22ed30239e49b8bb9
leopold <leopold.lacroix@yahoo.fr>
2022-11-25 08:45:49 +0100

    reset to adjency (performs better)

--- node_groupement.py
+++ node_groupement.py
@@ -1,20 +1,17 @@
 # %%
 import time
-from step_rep_geo import *
-
-
-
+from step_rep import *
 
 
 # def geo_plot_from_step_rep(step_rep:step_rep):
 #     geo_df = pd.DataFrame(
 #     dict([
-#         (node_name_col, step_rep.get_groups().to_list()),
+#         (group_name_col, step_rep.get_groups().to_list()),
 #         (group_weight, step_rep.geo_df[weight_col])
 #     ])
-#     ).rename_axis(group_col).reset_index().explode(node_name_col).reset_index()
+#     ).rename_axis(group_col).reset_index().explode(group_name_col).reset_index()
 
-#     geo_df[weight_col] = geo_df[weight_col][geo_df[node_name_col]].tolist()
+#     geo_df[weight_col] = geo_df[weight_col][geo_df[group_name_col]].tolist()
 #     geo_df[group_index_col] = geo_df[group_index_col].astype(int)
 
 #     # is_group = geo_df[group_col].str.contains(',')
@@ -22,30 +19,64 @@ from step_rep_geo import *
 #     # geo_df.loc[is_group, "color"] = geo_df[is_group][group_index_col].astype(int)  + 1
 #     geo_plot(geo_df, f'{step_rep}')
 
-# %%
-def next_try_all_possible(step: step_rep_geo) -> list[step_rep_geo]:
+def next_try_random_best(step: step_rep):
     next_steps_to_try = []
-    nodes_sorted_by_weight = step.nodes_sorted_by_weight()
-
-    for node in nodes_sorted_by_weight:
-        node_connections = step.get_node_connections(node)
-
-        if len(node_connections) != 0:
-            for node_connection in node_connections:
-                next_step = step.fuse([node, node_connection])
+    groups:np.ndarray = np.random.choice(step.group_ids_lower_that_can_improve_by_weight, 3, replace=False)
+    
+    for group in groups:
+        group_connections = step.get_group_connections_respecting_objective(group)
+        
+        if len(group_connections) != 0:
+            group_connections = np.random.choice(step.group_ids_lower_that_can_improve_by_weight, 3, replace=False)
+            for group_connection in group_connections:
+                next_step = step.fuse([group, group_connection])
                 if next_step.loss <= step.loss:
                     next_steps_to_try.append(next_step)
 
     return next_steps_to_try
 
-def next_try_connection_closest_to_obj(step: step_rep_geo) -> list[step_rep_geo]:
-    next_steps_to_try = []
-    nodes_sorted_by_weight = step.nodes_sorted_by_weight()
 
-    for node in nodes_sorted_by_weight:
-        node_connections = step.node_connections_respecting_objective(node)
-        if len(node_connections) != 0:
-            next_step = step.fuse([node, node_connections[0]])
+
+def next_try_small_bigs(step: step_rep):
+    next_steps_to_try = []
+
+    group = step.group_ids_lower_that_can_improve_by_weight[0]
+    group_connections = step.get_group_connections_respecting_objective(group)
+
+    if len(group_connections) != 0:
+        for group_connection_i in range(min(5, group_connections.shape[0])):
+            group_connection = group_connections[group_connection_i]
+            next_step = step.fuse([group, group_connection])
+            if next_step.loss <= step.loss:
+                next_steps_to_try.append(next_step)
+
+    return next_steps_to_try
+
+def next_try_smalls_bigs(step: step_rep) -> list[step_rep]:
+    next_steps_to_try = []
+
+    for group_i in range(min(3, step.group_ids_lower_that_can_improve_by_weight.shape[0])):
+        group = step.group_ids_lower_that_can_improve_by_weight[group_i]
+        group_connections = step.get_group_connections_respecting_objective(group)
+
+        if len(group_connections) != 0:
+            for group_connection_i in range(min(3, group_connections.shape[0])):
+                group_connection = group_connections[group_connection_i]
+                next_step = step.fuse([group, group_connection])
+                if next_step.loss <= step.loss:
+                    next_steps_to_try.append(next_step)
+
+    return next_steps_to_try
+
+def next_try_smalls_big(step: step_rep) -> list[step_rep]:
+    next_steps_to_try = []
+    groups_sorted_by_weight = step.group_ids_lower_that_can_improve_by_weight
+
+    for group_i in range(min(5, groups_sorted_by_weight.shape[0])):
+        group = groups_sorted_by_weight[group_i]
+        group_connections = step.get_group_connections_respecting_objective(group)
+        if len(group_connections) != 0:
+            next_step = step.fuse([group, group_connections[0]])
             if next_step.loss <= step.loss:
                 next_steps_to_try.append(next_step)
 
@@ -53,8 +84,7 @@ def next_try_connection_closest_to_obj(step: step_rep_geo) -> list[step_rep_geo]
 
 
 
-# %%
-def explore_problem(next_steps_method, geo_df = geo_df, weight_objective = weight_objective):
+def explore_problem(next_steps_method, constructor = step_rep, df =  adjency_df, weight_objective = weight_objective):
     def info():
         print(f"len(next_steps): {len(next_steps)}")
         print(f"len(tested_steps): {len(tested_steps)}")
@@ -65,11 +95,11 @@ def explore_problem(next_steps_method, geo_df = geo_df, weight_objective = weigh
     print(f"testing {next_steps_method.__name__}")
     start_time:float = time.time()
 
-    tested_steps:list[step_rep_geo] = []
-    next_steps: list[step_rep_geo] = [step_rep_geo(geo_df = geo_df, weight_objective = weight_objective, deepness = 0)]
+    tested_steps:list[constructor] = []
+    next_steps: list[constructor] = [constructor(df = df, weight_objective = weight_objective, deepness = 0)]
 
     while len(next_steps) != 0:
-        step: step_rep_geo = next_steps.pop(0)
+        step: constructor = next_steps.pop(0)
 
         already_tested = len(tested_steps) and any([tested_step == step for tested_step in tested_steps])
         if already_tested:
@@ -83,7 +113,7 @@ def explore_problem(next_steps_method, geo_df = geo_df, weight_objective = weigh
         if not step.can_improve:
             continue            
 
-        if len(tested_steps) - np.argmin(tested_steps) > 200 or elapsed_time_s > 5: #
+        if len(tested_steps) - np.argmin(tested_steps) > 200 or elapsed_time_s > 20: #
             break
 
         next_steps_to_try:list[str] = next_steps_method(step)
@@ -93,16 +123,16 @@ def explore_problem(next_steps_method, geo_df = geo_df, weight_objective = weigh
         next_steps += next_steps_to_try
         next_steps.sort()
 
-
     info()
     
     return tested_steps + next_steps
 
-
-explored_closest:list[step_rep_geo] = explore_problem(next_try_connection_closest_to_obj)
-explored_all:list[step_rep_geo] = explore_problem(next_try_all_possible)
+explored_random_best:list[step_rep] = explore_problem(next_try_random_best)
+explored_small_bigs:list[step_rep] = explore_problem(next_try_small_bigs)
+explored_smalls_big:list[step_rep] = explore_problem(next_try_smalls_big)
+explored_smalls_bigs:list[step_rep] = explore_problem(next_try_smalls_bigs)
 # %%
-def plot_loss(step_list_dict: dict[str,list[step_rep_geo]]):
+def plot_loss(step_list_dict: dict[str,list[step_rep]]):
     res_dfs: list[pd.DataFrame] = []
     titles:list[str] = []
     for strat_name, step_list in step_list_dict.items():
@@ -121,27 +151,17 @@ def plot_loss(step_list_dict: dict[str,list[step_rep_geo]]):
         data_frame = pd.concat(res_dfs),
         y = 'loss',
         color = "strategy",
-        title = "-".join(titles)
+        title = " | ".join(titles)
     )
     fig.show()
 
-plot_loss({"closest" : explored_closest, "all": explored_all})
+plot_loss({"smalls_big" : explored_smalls_big, "smalls_bigs": explored_smalls_bigs, "small_bigs": explored_small_bigs, "random_best": explored_random_best})
 
 
-best_result: step_rep_geo = np.min(explored_all + explored_closest)
-geo_df = best_result.geo_df
-best_result.plot('Best')
+best_result: step_rep = np.min(explored_smalls_bigs + explored_smalls_big + explored_small_bigs)
+adjency_df = best_result.adjency_df
+best_result.plot(f'Best {best_result}')
 
 
-# fig = go.Figure(
-#     data = go.Choropleth(
-#         z = geo_df[group_index_col], 
-#         geojson = geo_dict,
-#         locations = geo_df[node_name_col], 
-#         featureidkey = "properties.code",
-#         marker_line_color='white',
-#         # color_continuous_scale = "Reds",
-#         # range_color=(0, 12),
-#     )
-# )
+
 # %%
